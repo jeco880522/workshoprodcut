4ff034d335e57454e676a564b7339a1aa4372339
jeco880522 <juan.contreras.ospina@unillanos.edu.co>
2023-08-10 20:45:20 -0500

    añadiendo modularizacion

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { getProducts, postProduct, putProduct, deleteProduct } from "../js/services.js";
+
 const $d = document;
 const $table = $d.querySelector(".crud-table");
 const $form = $d.querySelector(".crud-form");
@@ -6,31 +8,22 @@ const $template = $d.getElementById("crud-template").content;
 const $fragement = $d.createDocumentFragment();
 
 const getAll = async () => {
-    try {
-        let res = await axios.get("http://localhost:3000/productos/")
-        let json = await res.data;
-        json.forEach(el => {
-            $template.querySelector(".name").textContent = el.nombre;
-            $template.querySelector(".proveed").textContent = el.proveedor;
-            $template.querySelector(".categori").textContent = el.categoria;
-            $template.querySelector(".valor").textContent = el.precio;
-            $template.querySelector(".edit").dataset.id = el.id;
-            $template.querySelector(".edit").dataset.name = el.nombre;
-            $template.querySelector(".edit").dataset.proveed = el.proveedor;
-            $template.querySelector(".edit").dataset.categori = el.categoria;
-            $template.querySelector(".edit").dataset.valor = el.precio;
-            $template.querySelector(".delete").dataset.id = el.id;
-
-            let $clone = $d.importNode($template, true);
-
-            $fragement.appendChild($clone);
-        });
-
-        $table.querySelector("tbody").appendChild($fragement);
-    } catch (error) {
-        let message = error.statusText || "Ocurrió un error";
-        $table.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
-    }
+    let json  =  await getProducts();
+    json.forEach(el => {
+        $template.querySelector(".name").textContent = el.nombre;
+        $template.querySelector(".proveed").textContent = el.proveedor;
+        $template.querySelector(".categori").textContent = el.categoria;
+        $template.querySelector(".valor").textContent = el.precio;
+        $template.querySelector(".edit").dataset.id = el.id;
+        $template.querySelector(".edit").dataset.name = el.nombre;
+        $template.querySelector(".edit").dataset.proveed = el.proveedor;
+        $template.querySelector(".edit").dataset.categori = el.categoria;
+        $template.querySelector(".edit").dataset.valor = el.precio;
+        $template.querySelector(".delete").dataset.id = el.id;
+        let $clone = $d.importNode($template, true);
+        $fragement.appendChild($clone);
+    });
+    $table.querySelector("tbody").appendChild($fragement);
 }
 
 $d.addEventListener("DOMContentLoaded", getAll);
@@ -38,58 +31,37 @@ $d.addEventListener("DOMContentLoaded", getAll);
 
 $d.addEventListener("submit", async e => {
     if (e.target === $form) {
-
         e.preventDefault();
-
         if (!e.target.id.value) {
-            try {
-                let options = {
-                    method: "POST",
-                    headers: {
-                        "Content-type": "application/json; charset=utf-8"
-                    },
-                    data: JSON.stringify({
-                        nombre: e.target.nombre.value,
-                        proveedor: e.target.proveedor.value,
-                        categoria: e.target.categoria.value,
-                        precio: e.target.precio.value
-                    })
-                };
-
-                let res = await axios("http://localhost:3000/productos/", options)
-                let json = await res.data;
-
-                location.reload();
-            } catch (error) {
-                
-                let message = error.statusText || "Ocurrió un error";
-                $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
-            }
+            let options = {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json; charset=utf-8"
+                },
+                data: JSON.stringify({
+                    nombre: e.target.nombre.value,
+                    proveedor: e.target.proveedor.value,
+                    categoria: e.target.categoria.value,
+                    precio: e.target.precio.value
+                })
+            };
+            let json = await postProduct(options);
+            location.reload();
         } else {
-            try {
-                let options = {
-                    method: "PUT",
-                    headers: {
-                        "Content-type": "application/json; charset=utf-8"
-                    },
-                    data: JSON.stringify({
-                        nombre: e.target.nombre.value,
-                        proveedor: e.target.proveedor.value,
-                        categoria: e.target.categoria.value,
-                        precio: e.target.precio.value
-                    })
-                };
-
-                let res = await axios(`http://localhost:3000/productos/${e.target.id.value}`, options)
-                let json = await res.data;
-
-                location.reload();
-            } catch (error) {
-
-                let message = error.statusText || "Ocurrió un error";
-                $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
-                
-            }
+            let options = {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json; charset=utf-8"
+                },
+                data: JSON.stringify({
+                    nombre: e.target.nombre.value,
+                    proveedor: e.target.proveedor.value,
+                    categoria: e.target.categoria.value,
+                    precio: e.target.precio.value
+                })
+            };
+            let json = await putProduct(e,options);
+            location.reload();
         }
     }
 })
@@ -103,29 +75,17 @@ $d.addEventListener("click", async e => {
         $form.precio.value = e.target.dataset.valor;
         $form.id.value = e.target.dataset.id;
     }
-
     if (e.target.matches(".delete")){
         let confirmacion = confirm("¿Estás seguro que deseas eliminar el elemnto seleccionado?")
-        
         if (confirmacion) {
-            try {
-                
-                let options = {
-                    method: "DELETE",
-                    headers: {
-                        "Content-type": "application/json; charset=utf-8"
-                    }
-                };
-
-                let res = await axios(`http://localhost:3000/productos/${e.target.dataset.id}`, options)
-                let json = await res.data;
-
-                location.reload();
-            } catch (error) {
-                
-                let message = error.statusText || "Ocurrió un error";
-                $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
-            }
+            let options = {
+                method: "DELETE",
+                headers: {
+                    "Content-type": "application/json; charset=utf-8"
+                }
+            };
+            let json = await deleteProduct(e,options);
+            location.reload();
         }
     }
 })
---
+++ js/services.js
@@ -0,0 +1,44 @@
+export async function getProducts (){
+    try {
+        let res = await axios.get("http://localhost:3000/productos/")
+        let json = await res.data;
+        return json;
+    } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $table.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
+    }
+}
+
+export async function postProduct(options){
+    try {
+        let res = await axios("http://localhost:3000/productos/", options)
+        let json = await res.data;
+        return json;
+    } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
+    }
+}
+
+export async function putProduct(e,options){
+    try {
+        let res = await axios(`http://localhost:3000/productos/${e.target.id.value}`, options)
+        let json = await res.data;
+        return json;
+    } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
+        
+    }
+}
+
+export async function deleteProduct(e,options){
+    try {
+        let res = await axios(`http://localhost:3000/productos/${e.target.dataset.id}`, options)
+        let json = await res.data;
+        return json;
+    } catch (error) {
+        let message = error.statusText || "Ocurrió un error";
+        $form.insertAdjacentHTML("afterend", `Error: ${error.status}: ${message}`);
+    }
+}
