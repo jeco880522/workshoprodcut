4097587a419149e0b8ad4212db806d0cf1f0b8e5
Juan Contreras <101781116+jeco880522@users.noreply.github.com>
2024-02-06 22:46:15 -0500

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 export async function getProducts (){
     try {
-        let res = await axios.get("https://github.com/jeco880522/workshoproduct/productos/")
+        let res = await axios.get("https://raw.githubusercontent.com/jeco880522/workshoproduct/main/productos/db.json")
         let json = await res.data;
         return json;
     } catch (error) {
